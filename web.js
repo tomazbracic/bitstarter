a645a0d6180670d6e5114889c256d9d96bc2a304
Ubuntu <ubuntu@ip-10-48-67-123.eu-west-1.compute.internal>
2013-07-10 13:06:24 +0000

    popravek fs funkcije

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('./index.html', utf-8)
+  var content = fs.readFileSync('./index.html', 'utf8')
   response.send(content);
 });
 
